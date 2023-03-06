fd842746ef9d5e3753d4ff37c195243f41d862f3
Daniel Bukowski <daniel.bukowski@expereo.com>
2023-03-06 11:58:15 +0100

    Added Beam analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import { NotificationsProvider } from '@mantine/notifications'
+import Script from 'next/script'
+
 import App from '../components/App'
 import RootStyleRegistry from './emotion'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en-US">
+      <Script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="87b2bb7d-c09d-483f-a84f-fce4a805b5b6" async />
       <body>
         <RootStyleRegistry>
           <NotificationsProvider position='top-right'>
